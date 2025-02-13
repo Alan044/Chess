c7061721968a4e840f9bdfc35112e46f3a51fed0
Alan044 <alanmohamadi@outlook.com>
2025-02-13 10:43:23 +0100

    fixade läckorna och errors, hade i++ istället för j++ i destroy_board loopen

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <threads.h>
 
 #define EIGHT 8
 typedef struct pieces pieces_t;
@@ -36,8 +37,9 @@ void empty_the_board(board_t*board)
 {
     for (int i = 0; i < EIGHT; i++) {
         for (int j = 0; j < EIGHT; j++) {
-            board->array[i][j] = calloc(1, sizeof(positions_t*));
-            board->array[i][j]->id = calloc(1, sizeof(id_of_board_t*));
+            board->array[i][j] = calloc(1, sizeof(positions_t));
+            board->array[i][j]->id = calloc(1, sizeof(id_of_board_t));
+            board->array[i][j]->status = false;
         }
     }
 }
@@ -58,7 +60,7 @@ void populate_pawns(board_t *board)
 {
     for (int j = 0; j < EIGHT; j++) {
         pieces_t* piece = calloc(1, sizeof(pieces_t));
-        piece->name_of_piece = calloc(1, sizeof(char*));
+        // piece->name_of_piece = calloc(1, sizeof(char*));
         piece->name_of_piece = "P";
         piece->points = 1;
         board->array[1][j]->piece = piece;
@@ -67,7 +69,7 @@ void populate_pawns(board_t *board)
     }
     for (int j = 0; j < EIGHT; j++) {
         pieces_t* piece = calloc(1, sizeof(pieces_t));
-        piece->name_of_piece = calloc(1, sizeof(char*));
+        // piece->name_of_piece = calloc(1, sizeof(char*));
         piece->name_of_piece = "P";
         piece->points = 1;
         board->array[6][j]->piece = piece;
@@ -80,7 +82,7 @@ void populate_rooks(board_t *board)
 {  
     
     pieces_t* rook_tl = calloc(1, sizeof(pieces_t));
-    rook_tl->name_of_piece = calloc(1, sizeof(char*));
+    // rook_tl->name_of_piece = calloc(1, sizeof(char*));
     rook_tl->name_of_piece = "R";
     rook_tl->points = 5;
     board->array[0][0]->piece = rook_tl;
@@ -88,7 +90,7 @@ void populate_rooks(board_t *board)
     board->array[0][0]->piece->colour = "bl";
 
     pieces_t* rook_tr = calloc(1, sizeof(pieces_t));
-    rook_tr->name_of_piece = calloc(1, sizeof(char*));
+    // rook_tr->name_of_piece = calloc(1, sizeof(char*));
     rook_tr->name_of_piece = "R";
     rook_tr->points = 5;
     board->array[0][7]->piece = rook_tr; 
@@ -96,7 +98,7 @@ void populate_rooks(board_t *board)
     board->array[0][7]->piece->colour = "bl";
 
     pieces_t* rook_dl = calloc(1, sizeof(pieces_t));
-    rook_dl->name_of_piece = calloc(1, sizeof(char*));
+    // rook_dl->name_of_piece = calloc(1, sizeof(char*));
     rook_dl->name_of_piece = "R";
     rook_dl->points = 5;
     board->array[7][7]->piece = rook_dl;
@@ -104,7 +106,7 @@ void populate_rooks(board_t *board)
     board->array[7][7]->piece->colour = "wh";
 
     pieces_t* rook_dr = calloc(1, sizeof(pieces_t));
-    rook_dr->name_of_piece = calloc(1, sizeof(char*));
+    // rook_dr->name_of_piece = calloc(1, sizeof(char*));
     rook_dr->name_of_piece = "R";
     rook_dr->points = 5;
     board->array[7][0]->piece = rook_dr;
@@ -115,7 +117,7 @@ void populate_rooks(board_t *board)
 void populate_horse(board_t *board)
 {
     pieces_t* horse_tl = calloc(1, sizeof(pieces_t));
-    horse_tl->name_of_piece = calloc(1, sizeof(char*));
+    // horse_tl->name_of_piece = calloc(1, sizeof(char*));
     horse_tl->name_of_piece = "H";
     horse_tl->points = 3;
     board->array[0][1]->status = true;
@@ -123,7 +125,7 @@ void populate_horse(board_t *board)
     board->array[0][1]->piece->colour = "bl";
 
     pieces_t* horse_tr = calloc(1, sizeof(pieces_t));
-    horse_tr->name_of_piece = calloc(1, sizeof(char*));
+    // horse_tr->name_of_piece = calloc(1, sizeof(char*));
     horse_tr->name_of_piece = "H";
     horse_tr->points = 3;
     board->array[0][6]->status = true;
@@ -131,7 +133,7 @@ void populate_horse(board_t *board)
     board->array[0][6]->piece->colour = "bl";
 
     pieces_t* horse_dl = calloc(1, sizeof(pieces_t));
-    horse_dl->name_of_piece = calloc(1, sizeof(char*));
+    // horse_dl->name_of_piece = calloc(1, sizeof(char*));
     horse_dl->name_of_piece = "H";
     horse_dl->points = 3;
     board->array[7][6]->status = true;
@@ -139,7 +141,7 @@ void populate_horse(board_t *board)
     board->array[7][6]->piece->colour = "wh";
 
     pieces_t* horse_dr = calloc(1, sizeof(pieces_t));
-    horse_dr->name_of_piece = calloc(1, sizeof(char*));
+    // horse_dr->name_of_piece = calloc(1, sizeof(char*));
     horse_dr->name_of_piece = "H";
     horse_dr->points = 3;
     board->array[7][1]->status = true;
@@ -150,7 +152,7 @@ void populate_horse(board_t *board)
 void populate_bishop(board_t *board)
 {
     pieces_t* bishop_tl = calloc(1, sizeof(pieces_t));
-    bishop_tl->name_of_piece = calloc(1, sizeof(char*));
+    // bishop_tl->name_of_piece = calloc(1, sizeof(char*));
     bishop_tl->name_of_piece = "B";
     bishop_tl->points = 3;
     board->array[0][2]->piece = bishop_tl;
@@ -158,7 +160,7 @@ void populate_bishop(board_t *board)
     board->array[0][2]->piece->colour = ("bl");
 
     pieces_t* bishop_tr = calloc(1, sizeof(pieces_t));
-    bishop_tr->name_of_piece = calloc(1, sizeof(char*));
+    // bishop_tr->name_of_piece = calloc(1, sizeof(char*));
     bishop_tr->name_of_piece = "B";
     bishop_tr->points = 3;
     board->array[0][5]->piece = bishop_tr;
@@ -166,7 +168,7 @@ void populate_bishop(board_t *board)
     board->array[0][5]->piece->colour = ("bl");
 
     pieces_t* bishop_dl = calloc(1, sizeof(pieces_t));
-    bishop_dl->name_of_piece = calloc(1, sizeof(char*));
+    // bishop_dl->name_of_piece = calloc(1, sizeof(char*));
     bishop_dl->name_of_piece = "B";
     bishop_dl->points = 3;
     board->array[7][5]->piece = bishop_dl;
@@ -174,7 +176,7 @@ void populate_bishop(board_t *board)
     board->array[7][5]->piece->colour = ("wh");
 
     pieces_t* bishop_dr = calloc(1, sizeof(pieces_t));
-    bishop_dr->name_of_piece = calloc(1, sizeof(char*));
+    // bishop_dr->name_of_piece = calloc(1, sizeof(char*));
     bishop_dr->name_of_piece = "B";
     bishop_dr->points = 3;
     board->array[7][2]->piece = bishop_dr;
@@ -185,7 +187,7 @@ void populate_bishop(board_t *board)
 void populate_kings_and_queens(board_t *board)
 {
     pieces_t* Queen_bl = calloc(1, sizeof(pieces_t));
-    Queen_bl->name_of_piece = calloc(1, sizeof(char*));
+    // Queen_bl->name_of_piece = calloc(1, sizeof(char*));
     Queen_bl->name_of_piece = "Q";
     Queen_bl->points = 9;
     board->array[0][3]->piece = Queen_bl; 
@@ -193,7 +195,7 @@ void populate_kings_and_queens(board_t *board)
     board->array[0][3]->piece->colour = ("bl");
 
     pieces_t* Queen_wh = calloc(1, sizeof(pieces_t));
-    Queen_wh->name_of_piece = calloc(1, sizeof(char*));
+    // Queen_wh->name_of_piece = calloc(1, sizeof(char*));
     Queen_wh->name_of_piece = "Q";
     Queen_wh->points = 9;
     board->array[7][3]->piece = Queen_wh; 
@@ -201,7 +203,7 @@ void populate_kings_and_queens(board_t *board)
     board->array[7][3]->piece->colour = ("wh"); 
     
     pieces_t* King_bl = calloc(1, sizeof(pieces_t));
-    King_bl->name_of_piece = calloc(1, sizeof(char*));
+    // King_bl->name_of_piece = calloc(1, sizeof(char*));    
     King_bl->name_of_piece = "K";
     King_bl->points = 0;
     board->array[0][4]->piece = King_bl; 
@@ -209,7 +211,7 @@ void populate_kings_and_queens(board_t *board)
     board->array[0][4]->piece->colour = ("bl"); 
 
     pieces_t* King_wh = calloc(1, sizeof(pieces_t));
-    King_wh->name_of_piece = calloc(1, sizeof(char*));
+    // King_wh->name_of_piece = calloc(1, sizeof(char*));
     King_wh->name_of_piece = "K";
     King_wh->points = 0;
     board->array[7][4]->piece = King_wh;
@@ -253,7 +255,6 @@ void print_board(board_t* board)
             if (board->array[i][j]->status) {
                 board->total_pieces++;
                 printf("%s(%s)", board->array[i][j]->piece->name_of_piece, board->array[i][j]->piece->colour);
-                
                 continue;
             }
             printf("  X  ");
@@ -262,10 +263,30 @@ void print_board(board_t* board)
     printf("\n");
     printf("total amont of pieces on the board %d \n", board->total_pieces);
 }
+void destroy_board(board_t *board)
+{
+    for (int i = 0; i < EIGHT; i++) {
+        
+        for (int j = 0; j < EIGHT; j++) {
+            positions_t *current = board->array[i][j];
+            if (current->piece) {
+                // free(current->piece->name_of_piece);
+                free(current->piece);
+                free(current->id);
+                free(current);
+                continue;
+            }
+            free(current->id);
+            free(current);
+        }
+    }
+    free(board->array);
+}
 
 int main(void)
 {
     board_t *board = create_board();
     print_board(board);
+    destroy_board(board);
     return 0;
 }
--- makefile
+++ makefile
@@ -7,6 +7,8 @@ run: main
 	clear
 	./main
 
+memcheck: main
+	valgrind --track-origins=yes  --leak-check=full --show-leak-kinds=all -s ./main
 clean:
 	rm main
 	clear
